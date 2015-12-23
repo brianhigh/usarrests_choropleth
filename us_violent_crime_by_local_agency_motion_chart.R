9f9e2e5ddec5efef44868e47320798f8d6217c89
brianhigh <bhigh@live.com>
2015-12-22 18:28:59 -0800

    Added state to locations in motion chart.

--- us_violent_crime_by_local_agency_motion_chart.R
+++ us_violent_crime_by_local_agency_motion_chart.R
@@ -1,8 +1,11 @@
-# --------------------------------------------------------------------
+# -----------------------------------------------------------------------
 # Create a Google Motion Chart for Violent Crime Rate (per 100,000
 # people) reported by US local law enforcement agencies serving over
-# 100,000 people or more for the years 1985 - 2012.
-# --------------------------------------------------------------------
+# 100,000 people for the years 1985 - 2012. 
+#
+# Copyright 2015 Brian High (https://github.com/brianhigh) 
+# License: GNU GPL v3 http://www.gnu.org/licenses/gpl.txt
+# -----------------------------------------------------------------------
 
 # Close connections and clear objects.
 closeAllConnections()
@@ -20,18 +23,22 @@ file.name <- "all_us_states_local_agencies_fbi_crime_data.csv"
 filepath <- paste(datadir, file.name, sep="/")
 all.states.data <- read.csv(filepath, stringsAsFactors = FALSE)
 
+# Combine Agency and State into a single Location variable to use as an index.
+all.states.data$Location <- paste(all.states.data$Agency, 
+                                  all.states.data$State, sep=", ")
+
 # Subset by agencies in a particular State
 #crime <- all.states.data[all.states.data$State == "Washington",]
 
 # Subset the violent crimes.
 violent <- c("Year", "Murder_and_nonnegligent_manslaughter_rate",
              "Forcible_rape_rate", "Robbery_rate", "Aggravated_assault_rate",
-             "Violent_Crime_rate", "Agency", "Population")
+             "Violent_Crime_rate", "Location", "Population")
 violent.crime <- all.states.data[, violent]
 
 # Shorten some crime names for shorter plot labels and capitalize them.
 violent <- c("Year", "Murder", "Rape", "Robbery", "Assault", "Violent",
-             "Agency", "Population")
+             "Location", "Population")
 names(violent.crime) <- violent
 
 # Subset by Population >= 100,000
@@ -40,12 +47,12 @@ violent.crime <- violent.crime[violent.crime$Population >= 100000,]
 # Remove cases containing NAs
 violent.crime <- na.omit(violent.crime)
 
-# Remove duplicate cases of the index variabes: Year and Agency
-index <- which(duplicated(violent.crime[,c("Agency", "Year")]))
+# Remove duplicate cases of the index variabes: Year and Location
+index <- which(duplicated(violent.crime[,c("Location", "Year")]))
 violent.crime <- violent.crime[-index, ]
 
 # Make the visualization
-plot(gvisMotionChart(data = violent.crime, idvar = "Agency", 
+plot(gvisMotionChart(data = violent.crime, idvar = "Location", 
                      timevar = "Year", colorvar = "Violent",
                      xvar = "Robbery", yvar = "Assault", sizevar="Population",
                      options=list(width=800, height=600)))
